a8c3104c73327bfd631322a9c9dc87fda11c5c13
Mohamed Aymen Daassi <m.daassi@innopolis.university>
2023-07-12 17:00:43 +0300

    fix lint install

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,7 +29,7 @@ module.exports = {
     'svelte3/ignore-styles': () => true
   },
   rules: {
-    "typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-unsafe-member-access": "off",
     "@typescript-eslint/no-unsafe-call": "off"
   },
